9ee4d4d48c4192a48e7c077ea104c0cd1d1cf37f
SmushyTaco <smushytaco@gmail.com>
2023-06-15 15:44:56 -0700

    1.20.1

---
+++ Expanded Bow Enchanting/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("fabric-loom")
+    kotlin("jvm").version(System.getProperty("kotlin_version"))
+}
+base { archivesName.set(project.extra["archives_base_name"] as String) }
+version = project.extra["mod_version"] as String
+group = project.extra["maven_group"] as String
+repositories {
+    maven("https://maven.shedaniel.me/")
+    maven("https://maven.terraformersmc.com/")
+    maven("https://jitpack.io")
+}
+dependencies {
+    minecraft("com.mojang", "minecraft", project.extra["minecraft_version"] as String)
+    mappings("net.fabricmc", "yarn", project.extra["yarn_mappings"] as String, null, "v2")
+    modImplementation("net.fabricmc", "fabric-loader", project.extra["loader_version"] as String)
+    modImplementation("net.fabricmc.fabric-api", "fabric-api", project.extra["fabric_version"] as String)
+    modImplementation("net.fabricmc", "fabric-language-kotlin", project.extra["fabric_language_kotlin_version"] as String)
+    modImplementation("com.terraformersmc", "modmenu", project.extra["mod_menu_version"] as String)
+    modApi("me.shedaniel.cloth", "cloth-config-fabric", project.extra["cloth_config_version"] as String) { exclude("net.fabricmc.fabric-api") }
+    include(implementation(annotationProcessor("com.github.LlamaLad7", "MixinExtras", project.extra["mixin_extras_version"] as String))!!)
+}
+tasks {
+    val javaVersion = JavaVersion.toVersion((project.extra["java_version"] as String).toInt())
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+        options.release.set(javaVersion.toString().toInt())
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions { jvmTarget = javaVersion.toString() } }
+    jar { from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } } }
+    processResources {
+        filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.extra["mod_version"] as String, "fabricloader" to project.extra["loader_version"] as String, "fabric_api" to project.extra["fabric_version"] as String, "fabric_language_kotlin" to project.extra["fabric_language_kotlin_version"] as String, "minecraft" to project.extra["minecraft_version"] as String, "java" to project.extra["java_version"] as String, "modmenu" to project.extra["mod_menu_version"] as String, "clothconfig" to project.extra["cloth_config_version"] as String, "mixinextras" to project.extra["mixin_extras_version"] as String)) }
+        filesMatching("*.mixins.json") { expand(mutableMapOf("java" to project.extra["java_version"] as String)) }
+    }
+    java {
+        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+        withSourcesJar()
+    }
+}
