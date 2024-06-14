a344e0747a0fbd7e81c919b3da6983a0dc0c7c84
Nikan Radan <personal@nikanradan.com>
2024-06-14 02:32:24 -0700

    1.21

--- Expanded Bow Enchanting/src/main/java/com/smushytaco/expanded_bow_enchanting/mixin/BowModification.java
+++ Expanded Bow Enchanting/src/main/java/com/smushytaco/expanded_bow_enchanting/mixin/BowModification.java
@@ -2,21 +2,29 @@ package com.smushytaco.expanded_bow_enchanting.mixin;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.smushytaco.expanded_bow_enchanting.ExpandedBowEnchanting;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.LuckEnchantment;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.tag.ItemTags;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 @Mixin(Enchantment.class)
 public abstract class BowModification {
-    @Shadow
-    @Final
-    private Enchantment.Properties properties;
     @ModifyReturnValue(method = "isAcceptableItem", at = @At("RETURN"))
-    public boolean isAcceptableItem(boolean original, ItemStack stack) {
-        return ((Enchantment) (Object) this) instanceof LuckEnchantment ? ExpandedBowEnchanting.INSTANCE.getConfig().getCanUseLootingOnBow() && properties.supportedItems() == ItemTags.SWORD_ENCHANTABLE && stack.getItem() instanceof BowItem || original : original;
+    public boolean hookIsAcceptableItem(boolean original, ItemStack stack) {
+        Enchantment enchantment = (Enchantment) (Object) this;
+        if (ExpandedBowEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.LOOTING)) {
+            if (!ExpandedBowEnchanting.INSTANCE.getConfig().getCanUseLootingOnBow() || !(stack.getItem() instanceof BowItem)) return original;
+            return true;
+        } else if (ExpandedBowEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.PIERCING)) {
+            if (!ExpandedBowEnchanting.INSTANCE.getConfig().getCanUsePiercingOnBow() || !(stack.getItem() instanceof BowItem)) return original;
+            return true;
+        } else if (ExpandedBowEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.MULTISHOT)) {
+            if (!ExpandedBowEnchanting.INSTANCE.getConfig().getCanUseMultishotOnBow() || !(stack.getItem() instanceof BowItem)) return original;
+            return true;
+        }
+        return original;
     }
+    @ModifyReturnValue(method = "canBeCombined", at = @At("RETURN"))
+    private static boolean hookCanBeCombined(boolean original, RegistryEntry<Enchantment> first, RegistryEntry<Enchantment> second) { return ExpandedBowEnchanting.INSTANCE.getConfig().getInfinityAndMendingCanBeMixed() && (first.matchesKey(Enchantments.INFINITY) && second.matchesKey(Enchantments.MENDING) || first.matchesKey(Enchantments.MENDING) && second.matchesKey(Enchantments.INFINITY)) || original; }
 }
