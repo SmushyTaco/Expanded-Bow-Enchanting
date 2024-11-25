bfc48cee12f337702a87566b3950b67b54d686cc
Nikan Radan <personal@nikanradan.com>
2024-11-25 15:09:48 -0800

    New Config

---
+++ Expanded Bow Enchanting/src/main/kotlin/com/smushytaco/expanded_bow_enchanting/ModConfiguration.kt
@@ -0,0 +1,15 @@
+package com.smushytaco.expanded_bow_enchanting
+import io.wispforest.owo.config.annotation.*
+@Modmenu(modId = ExpandedBowEnchanting.MOD_ID)
+@Config(name = ExpandedBowEnchanting.MOD_ID, wrapperName = "ModConfig")
+@Suppress("UNUSED")
+class ModConfiguration {
+    @JvmField
+    var infinityAndMendingCanBeMixed = true
+    @JvmField
+    var canUseLootingOnBow = true
+    @JvmField
+    var canUsePiercingOnBow = true
+    @JvmField
+    var canUseMultishotOnBow = true
+}
